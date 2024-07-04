7cb4c74119f33337d1a86f03ceba04f7ccd6f8d2
Akira Tomita <tomita@at-developers.com>
2024-07-05 08:41:21 +0900

    外部公開クラスの追加

---
+++ NpoiWrapper/Common/CellRange.cs
@@ -0,0 +1,111 @@
+﻿using NPOI.SS.Util;
+using System;
+
+namespace Developers.NpoiWrapper.Common
+{
+    /// <summary>
+    /// レンジアドレスクラス
+    /// </summary>
+    public class CellRange
+    {
+        #region "constructors"
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Range">A1形式のレンジアドレス(カンマ区切りで複数指定された場合は先頭のみ参照)</param>
+        public CellRange(string References)
+        {
+            string [] ReferenceList = References.Split(',');
+            Address = CellRangeAddress.ValueOf(ReferenceList[0]);
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="FirstRow">先頭行Index(1-base)</param>
+        /// <param name="LastRow">最終行Index(1-base)</param>
+        /// <param name="FirstColumn">先頭列Index(1-base)</param>
+        /// <param name="LastColumn">最終列Index(1-base)</param>
+        public CellRange(int RowIndex, int ColumnIndex)
+        {
+            Address = new CellRangeAddress(RowIndex, RowIndex, ColumnIndex, ColumnIndex);
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="FirstRow">先頭行Index(1-base)</param>
+        /// <param name="LastRow">最終行Index(1-base)</param>
+        /// <param name="FirstColumn">先頭列Index(1-base)</param>
+        /// <param name="LastColumn">最終列Index(1-base)</param>
+        public CellRange(int FirstRow, int LastRow, int FirstColumn, int LastColumn)
+        {
+            if (FirstRow >= 1 && LastRow >= 1 && FirstColumn >= 1 && LastColumn >= 1)
+            {
+                Address = new CellRangeAddress(FirstRow - 1, LastRow - 1, FirstColumn - 1, LastColumn - 1);
+            }
+            else
+            {
+                throw new ArgumentException("Index out of range.");
+            }
+        }
+
+        #endregion
+
+        #region "properties"
+
+        private CellRangeAddress Address { get; set; } = new CellRangeAddress(0, 0, 0, 0);
+
+        /// <summary>
+        /// A1形式のレンジアドレス
+        /// </summary>
+        public string Reference
+        {
+            get { return Address.FormatAsString(null, false); }
+        }
+
+        /// <summary>
+        /// この範囲に含まれるセルの個数
+        /// </summary>
+        public int CellCount
+        {
+            get { return Address.NumberOfCells; }
+        }
+
+        /// <summary>
+        /// 先頭行Index(1-base)
+        /// </summary>
+        public int FirstRowIndex
+        {
+            get { return Address.FirstRow + 1; }
+        }
+
+        /// <summary>
+        /// 最終行Index(1-base)
+        /// </summary>
+        public int LastRowIndex
+        {
+            get { return Address.LastRow + 1; }
+        }
+
+        /// <summary>
+        /// 先頭列Index(1-base)
+        /// </summary>
+        public int FirstColumnIndex
+        {
+            get { return Address.FirstColumn + 1; }
+        }
+
+        /// <summary>
+        /// 最終列Index(1-base)
+        /// </summary>
+        public int LastColumnIndex
+        {
+            get { return Address.LastColumn + 1; }
+        }
+
+        #endregion
+
+    }
+}
---
+++ NpoiWrapper/Common/CellReference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NPOI.SS.Util;
+
+namespace Developers.NpoiWrapper.Common
+{
+    public class ColumnReference
+    {
+        /// <summary>
+        /// A形式の列アドレスを列インデックスに変換(1-base)
+        /// </summary>
+        /// <param name="Reference"></param>
+        /// <returns></returns>
+        public static int StringToIndex(string Reference)
+        {
+            return CellReference.ConvertColStringToIndex(Reference) + 1;
+        }
+
+        /// <summary>
+        /// 列インデックス(1-base)をA形式の列アドレスに変換
+        /// </summary>
+        /// <param name="Index"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string IndexToString(int Index)
+        {
+            if ( Index >= 1)
+            {
+                return CellReference.ConvertNumToColString(Index - 1);
+            }
+            else
+            {
+                throw new ArgumentException("Column index out of range.");
+            }
+        }
+    }
+}
